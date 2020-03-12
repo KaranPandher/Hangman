32105731f51d7bc4ebadb5fc8aca2558f04b263e
Karan Pandher <karanpandher@live.ca>
2020-03-12 07:31:46 -0600

    Buttons Added

--- hangman.js
+++ hangman.js
@@ -1,4 +1,5 @@
-var cities = [ 
+//alert('Hello, World!');
+var cities = [ //Array of cities; choice of words 
     "london",
     "delhi",
     "tokyo",
@@ -8,7 +9,9 @@ var cities = [
     "chicago",
     "guangzhou",
     "bangkok",
-    "dubai"
+    "dubai",
+    "seoul",
+
 ]
 
 let answer = ' ';
@@ -17,24 +20,33 @@ let mistakes = 0;
 let guessed = [];
 let wordStatus = null; 
 
+//Pick a random word form the words arrray 
 function randomWord() {
     answer = cities[Math.floor(Math.random() * cities.length)];
 
 }
 
-function generateButtons() {
-    let buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split('').map(letter =>
-        `
-        <button 
-            class = "btn btn-lg btn-primary m-2" 
-            id ='` + letter + `
-            onClick="handleGuess('` + letter + `')"
-         >  
-          ` + letter + `
-         </button>
-        `);
-    document.getElementById('keyboard').innerHTML = buttonsHTML;
+// Set up Array for Answers; this shows how many leters are in the world. 
+
+var answerArray = [];
+for (var i = 0; i < cities.length; i++) {
+    answerArray[i] = "_";
 }
+
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+  }
 
 randomWord();
 generateButtons();
